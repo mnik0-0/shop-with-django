3e5278ee0ad8b45d0b338435d7a919be99132961
nikita <nikitamigdasev@gmail.com>
2020-07-16 14:56:30 +0300

    Fix travis

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -76,11 +76,9 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shop_db',
-        'USER': 'shop',
-        'PASSWORD': 'shop_p',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'travisdb',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
